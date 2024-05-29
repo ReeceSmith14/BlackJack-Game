464b93bd2224810365508bc08b751fa50e89ea8d
ReeceSmith14 <61851890+ReeceSmith14@users.noreply.github.com>
2024-05-29 17:48:51 +0000

    docs,chore and test: Solution for jest accreddited to user on stackoverflow, comments in js adjusted to JSDoc conventions and global variable tests done

--- assets/scripts/tests/game.test.js
+++ assets/scripts/tests/game.test.js
@@ -1,42 +1,42 @@
-// Imported modules for testing
-const { playerHand, playerTotal, dealerHand, dealerTotal, targetTotal, deck, blankCard } = require('../game');
 
-// Mocking the entire DOM for testing
+const {
+  playerHand,
+  dealerHand,
+  playerTotal,
+  dealerTotal,
+  targetTotal,
+  deck,
+  blankCard,
+  createDeck,
+  shuffleDeck,
+  calculateHandTotal,
+  determineWinner
+} = require("../game"); 
 
-beforeEach(() =>{
-  let fs = require("fs");
-  let fileContents = fs.readFileSync("index.html", "utf-8");
-  document.open();
-  document.write(fileContents);
-  document.close();
+describe("Global variables should be initialised correctly", () => {
+  test("playerHand should be an empty array", () => {
+    expect(playerHand).toEqual([]);
 });
-
-describe("Global game variables should be correctly initialised", () => {
-  test("playerHand should be defined and be an empty array", () => {
-	expect(playerHand).toEqual([]);
+test("dealerHand should be an empty array", () =>{
+  expect(dealerHand).toEqual([]);
+});
+test("playerTotal should be 0", () =>{ 
+  expect(playerTotal).toBe(0);
+});
+test("dealerTotal should be 0", () => { 
+  expect(dealerTotal).toBe(0);
+});
+test("targetTotal should be 21", () => { 
+  expect(targetTotal).toBe(21);
+});
+test("deck should be an empty array", () => {
+  expect(deck).toEqual([]);
+});
+test("blankCard should have no value, null suit and use the blank image", () => {
+  expect(blankCard).toEqual({
+    value: 0,
+    suit: null,
+    image: 'assets/images/cards/BACK.png'
   });
-
-  test("playerTotal should be defined and be 0", () => {
-    expect(playerTotal).toBe(0);
-  });
-
-  test("dealerHand should be defined and be an empty array", () => {
-    expect(dealerHand).toEqual([]);
-  });
-
-  test("dealerTotal should be defined and be 0", () => {
-    expect(dealerTotal).toBe(0);
-  });
-
-  test("targetTotal defined and be 21", () => {
-    expect(targetTotal).toBe(21);
-  });
-
-  test("deck should be defined and be an empty array initially", () => {
-    expect(deck).toEqual([]);
-  });
-
-  test("blankCard defined and have the correct structure", () => {
-    expect(blankCard).toEqual({value: 0, suit: null, image: "assets/images/cards/BACK.png"});
-  });
+});
 });
